ca42932c141c90ab9aaea39d34d3a6e23eaf1c69
danrey321 <dreyes9@ucmerced.edu>
2020-03-17 16:32:00 -0700

    Event Selection update

--- js/script.js
+++ js/script.js
@@ -59,3 +59,109 @@ $("#design").change(function(){
     }
 
 })
+
+/* 
+create variable to update cost
+create funmction to update cost
+create function, on check add cost to total cost, disable other conflicting events. 
+open source enable and disable function
+ https://stackoverflow.com/questions/17599984/how-to-enable-a-disabled-checkbox-dynamically
+
+*/
+function enable() {
+    document.getElementById("check").disabled= false;
+
+}
+function disable() {
+     document.getElementById("check").disabled= true;
+}
+
+let totalCost = 0;
+$(".activities").append('<div class="total-cost"></div>' );
+
+
+function costUpdate(update){
+    totalCost = totalCost + update;
+    $('div.total-cost').text("Total Cost: $" + totalCost)
+    
+}
+
+$("input[name='all']").change(function(){
+    $this = $(this)
+    if($this.is(":checked")){
+        costUpdate(200);
+    }else{
+        costUpdate(-200);
+    }
+
+});
+$("input[name='npm']").change(function(){
+    $this = $(this)
+    if($this.is(":checked")){
+        costUpdate(100);
+    }else{
+        costUpdate(-100);
+    }
+
+});
+$("input[name='build-tools']").change(function(){
+    $this = $(this)
+    if($this.is(":checked")){
+        costUpdate(100);
+    }else{
+        costUpdate(-100);
+    }
+
+});
+//https://api.jquery.com/prop/#entry-longdesc-1
+$("input[name=js-frameworks]").change(function(){
+    $this = $(this)
+    if($this.is(":checked")){
+        costUpdate(100);
+        $("input[name='express']").attr('disabled', true);
+    }
+    else{
+        costUpdate(-100);
+        $("input[name='express']").attr('disabled', false);
+    }
+
+});
+
+$("input[name=js-libs]").change(function(){
+    $this = $(this)
+    if($this.is(":checked")){
+        costUpdate(100);
+        $("input[name='node']").attr('disabled', true);
+    }
+    else{
+        costUpdate(-100);
+        $("input[name='node']").attr('disabled', false);
+    }
+
+});
+$("input[name=express]").change(function(){
+    $this = $(this)
+    if($this.is(":checked")){
+        costUpdate(100);
+        $("input[name='js-frameworks']").attr('disabled', true);
+    }
+    else{
+        costUpdate(-100);
+        $("input[name='js-frameworks']").attr('disabled', false);
+    }
+
+});
+$("input[name=node]").change(function(){
+    $this = $(this)
+    if($this.is(":checked")){
+        costUpdate(100);
+        $("input[name='js-libs']").attr('disabled', true);
+    }
+    else{
+        costUpdate(-100);
+        $("input[name='js-libs']").attr('disabled', false);
+    }
+
+});
+
+
